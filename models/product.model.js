101abc06c65d47a438560109ffce4b5f4d8fbcd4
Nicholas <danielnick210@gmail.com>
2026-05-26 13:28:31 +0100

    created the products model

---
+++ models/product.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true, unique: true },
+		price: { type: Number, required: true },
+		description: { type: String },
+		category: { type: String },
+		stock: { type: Number },
+		isAvailable: { type: Boolean, default: false },
+		image: { url: String, publicId: String },
+		createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+	},
+	{ timestamps: true },
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
