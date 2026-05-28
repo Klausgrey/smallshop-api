e7f6d7db98abd1903a6d10c8f44f815a8b085b5a
Nicholas <danielnick210@gmail.com>
2026-05-28 12:20:34 +0100

    fixed a bug in the auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,5 @@
 import pkg from "jsonwebtoken";
 const { verify } = pkg;
-const JWT_SECRET = process.env.JWT_SECRET;
 
 export const verifyToken = async (req, res, next) => {
 	const auth = req.headers.authorization;
@@ -10,8 +9,9 @@ export const verifyToken = async (req, res, next) => {
 	if (!token) return res.status(401).json({ message: "Invalid token format" });
 
 	try {
-		const decoded = verify(token, JWT_SECRET);
+		const decoded = verify(token, process.env.JWT_SECRET);
 		req.user = decoded;
+		console.log(decoded);
 		next();
 	} catch (err) {
 		next(err);
