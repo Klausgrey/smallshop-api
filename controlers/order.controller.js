ee0dbb77c4c0c577a42fad9914b3d1b8d65e7302
Nicholas <danielnick210@gmail.com>
2026-05-31 10:11:08 +0100

    creating the order controllers

--- controlers/order.controller.js
+++ controlers/order.controller.js
@@ -1,1 +1,28 @@
-import Order from "../models/order.model.js"
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
+
+export const createOrder = async (req, res, next) => {
+	const productId = req.params.id;
+	const quantity = req.body;
+	const userId = req.user.id;
+
+	try {
+		const product = await Product.findById(productId);
+
+		if (!product) return res.status(404).json({ message: "product not found" });
+		if (product.stock >= quantity)
+			return res.status(400).json({ message: "Not enough stock availble" });
+
+		const productPrice = product.price;
+		const userOrder = await Order.create({
+			userId,
+			productId,
+			quantity,
+			totalPrice: productPrice * quantity,
+		});
+		await Product.findByIdAndUpdate(productId, { $inc: { stock: -quantity } });
+		res.status(201).json(userOrder);
+	} catch (err) {
+		next(err);
+	}
+};
