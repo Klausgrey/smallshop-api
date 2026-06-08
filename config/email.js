00b4fcb368c4ebb1f3a2e1dda76990a2bc8f9bd5
Nicholas <danielnick210@gmail.com>
2026-06-08 19:27:59 +0100

    changed email config

--- config/email.js
+++ config/email.js
@@ -1,23 +1,55 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: process.env.EMAIL,
-		pass: process.env.EMAIL,
-	},
-});
+// const transporter = nodemailer.createTransport({
+// 	service: "gmail",
+// 	auth: {
+// 		user: process.env.EMAIL,
+// 		pass: process.env.EMAIL,
+// 	},
+// });
+
+// export const sendEmail = async ({ to, subject, text }) => {
+// 	await transporter.sendMail({
+// 		from: process.env.EMAIL,
+// 		to,
+// 		subject,
+// 		text,
+// 	});
+// };
+
+// export default transporter;
+
+import nodemailer from "nodemailer";
+
+let transporter;
+
+const initTransporter = async () => {
+	const testAccount = await nodemailer.createTestAccount();
+
+	transporter = nodemailer.createTransport({
+		host: "smtp.ethereal.email",
+		port: 587,
+		auth: {
+			user: testAccount.user,
+			pass: testAccount.pass,
+		},
+	});
+
+	console.log("Ethereal email:", testAccount.user);
+	console.log("Ethereal password:", testAccount.pass);
+};
+
+initTransporter();
 
 export const sendEmail = async ({ to, subject, text }) => {
-	await transporter.sendMail({
-		from: process.env.EMAIL,
+	const info = await transporter.sendMail({
+		from: "smallshop@test.com",
 		to,
 		subject,
 		text,
 	});
+
+	console.log("Preview URL:", nodemailer.getTestMessageUrl(info));
 };
-
-
-export default transporter
