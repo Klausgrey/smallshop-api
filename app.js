3a270671e3dfa5b357eb480c0fb0e79f51bf132c
Nicholas <danielnick210@gmail.com>
2026-06-10 04:50:21 +0100

    add the cors middleware

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 import express from "express";
 import authRoute from "./routes/auth.route.js";
-import productRoute from "./routes/product.route.js"
-import orderRoute from "./routes/order.route.js"
+import productRoute from "./routes/product.route.js";
+import orderRoute from "./routes/order.route.js";
 import errorHandler from "./middleware/error.middleware.js";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoute);
-app.use("/api", productRoute)
-app.use("/api", orderRoute)
+app.use("/api", productRoute);
+app.use("/api", orderRoute);
+app.use(cors());
 app.use(errorHandler);
 connectDB();
 
